b0dc32922b02e700b84cbc8d0af14695bd866a2b
Lương Văn Tú <luongvantuit@gmail.com>
2022-06-04 14:14:24 +0700

    aws ami filter arm64 architecture

--- main.tf
+++ main.tf
@@ -1,7 +1,55 @@
+terraform {
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+
 provider "aws" {
-  region = "ap-southeast-1"
+  profile = "default"
+  region  = "ap-southeast-1"
+}
+
+
+
+data "aws_ami" "ami" {
+  owners      = var.aws_ami_owners
+  most_recent = true
+
+  filter {
+    name   = "architecture"
+    values = ["arm64"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-arm64-server-*"]
+  }
 }
 
 resource "aws_instance" "instance" {
-  count = var.aws_ec2_instance_count
+  count         = var.aws_ec2_instance_count
+  ami           = data.aws_ami.ami.id
+  instance_type = var.aws_ec2_instance_type
+  key_name      = var.aws_ec2_key_name
+
+  tags = {
+    "Name" = "${var.aws_ec2_tag_name}-${count.index}",
+  }
 }
--- variables.tf
+++ variables.tf
@@ -3,3 +3,26 @@ variable "aws_ec2_instance_count" {
   default     = 1
   description = "Is quantity AWS EC2 instance"
 }
+
+
+variable "aws_ec2_key_name" {
+  type        = string
+  description = "Key pair name EC2 instance"
+}
+
+variable "aws_ec2_instance_type" {
+  type        = string
+  default     = "c6g.medium"
+  description = "Is instance type AWS EC2 instance"
+}
+
+variable "aws_ec2_tag_name" {
+  type        = string
+  default     = "EC2 instance"
+  description = "Is tag name EC2 instance"
+}
+
+variable "aws_ami_owners" {
+  type    = list(string)
+  default = ["099720109477"]
+}
